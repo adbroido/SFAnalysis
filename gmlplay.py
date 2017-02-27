460b9bb1bcf491e33e5ac8f20fe8a2ee19039775
Anna D. Broido <broidoa@gmail.com>
2017-02-27 14:59:09 -0700

    Add script to combine gmls

--- gmlplay.py
+++ gmlplay.py
@@ -1,3 +1,42 @@
 import igraph
+import glob
+import numpy as np
 
-g = igraph.read('test.gml')
+def myunion(graphs):
+    if not (all([graph.is_directed() for graph in graphs]) or all([not graph.is_directed() for graph in graphs])):
+        print "You can't mix directed and undirected graphs"
+    v = np.max([graph.vcount() for graph in graphs])
+    g = igraph.Graph(n=v, directed=graphs[1].is_directed())
+    for graph in graphs:
+        attrnames = graph.es.attributes()
+        start = g.ecount()
+        g.add_edges([e.tuple for e in graph.es])
+        stop = g.ecount()
+        for att in attrnames:
+            g.es[start:stop][att] = graph.es[att]
+    return g
+
+
+
+
+gmldpn2 = '/Users/annabroido/CU/testgmls/Social/Political/n2/'
+gmldpn3 = '/Users/annabroido/CU/testgmls/Social/Political/n3/'
+
+
+senatefpV = glob.glob(gmldpn2+'*enate*.gml')
+senatefpV += glob.glob(gmldpn3+'*enate*.gml')
+housefpV = glob.glob(gmldpn2+'*ouse*.gml')
+housefpV += glob.glob(gmldpn3+'*ouse*.gml')
+
+graphs = []
+for fp in senatefpV:
+    # read graph
+    g = igraph.read(fp)
+    # get snapshot id
+    fn = fp.split("/")[-1]
+    fnsplit = fn.split("_")
+    ind = fnsplit.index("Senate")+1
+    snap = fnsplit[ind]
+    # add to edges
+    g.es['snap'] = snap
+    graphs.append(g)
